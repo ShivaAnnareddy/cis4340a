58f9c0a1bada26dd92c9f3b63dda67d22e61fbe6
ShivaAnnareddy <166434919+ShivaAnnareddy@users.noreply.github.com>
2024-04-30 14:55:00 -0400

    Expressions (EXP)
    This is the file that deals with Rule 02.

---
+++ R02_XP00_J.java
@@ -0,0 +1,14 @@
+import java.io.File;
+
+public class FileDeletionHandler {
+
+    public void deleteFile() {
+        File someFile = new File("someFileName.txt");
+        // Do something with someFile
+        
+        if (!someFile.delete()) {
+            // Handle failure to delete the file
+            System.out.println("Failed to delete the file.");
+        }
+    }
+}
